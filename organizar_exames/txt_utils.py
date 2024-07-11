2db1585546d4377eaf229c3a1c2cf938d521d685
viniciussm07 <vini.soares.martins@gmail.com>
2024-07-11 18:31:16 -0300

    Feat: Função que remove linhas com determinadas strings dos arquivos TXT

---
+++ organizar_exames/txt_utils.py
@@ -0,0 +1,17 @@
+import file_processing
+
+def remove_lines_with_keywords(input_file, output_file):
+    keywords = ['Globulinas', 'urina', 'fezes', 'Urina', 'Fezes', 'URINA', 'FEZES', 'BIOQUÍMICA', 'HEMATOLOGIA', 'UROANÁLISE', 'MICROBIOLOGIA', 'Hemocultura', 'HERMES PARDINI', 'Tacrolimus', 'Brucelose', 'Citomegalovirus', 'Hepatite', 'HIV', 'HTLV', 'Trypanosoma', 'IMUNOLOGIA', 'VDRL', 'ABO-RhD', 'Fator Rh', 'Pesquisa de D fraco', '%', 'HEMODIÁLISE', 'Uréia Pós Hemodiálise', 'LAB. APOIO DB', 'Relação TTPA/Controle', 'Glicemia Média Estimada', 'HEMODIÁLISE']
+    with open(input_file, 'r', encoding='ISO-8859-1') as infile:
+        lines = infile.readlines()
+    
+    with open(output_file, 'w', encoding='utf-8') as outfile:
+        for line in lines:
+            if not any(keyword in line for keyword in keywords):
+                outfile.write(line)
+
+if __name__ == '__main__':
+    input_file_txt = input("Digite o caminho raiz dos arquivos txt organizados por dia: ")
+    output_txt_sem_linhas_inuteis = input("Digite o caminho para o txt sem linhas inúteis: ")
+    file_processing.process_files(input_file_txt, output_txt_sem_linhas_inuteis, remove_lines_with_keywords)
+    print("Linhas removidas com sucesso!")
