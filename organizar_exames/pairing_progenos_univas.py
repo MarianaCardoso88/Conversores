6cfcde829cf409d0609f4369c7d3e7f85fa9f185
viniciussm07 <vini.soares.martins@gmail.com>
2024-08-02 18:46:52 -0300

    Feat: pair agora recebe 3 parâmetros, os quais são nomes de arquivos e salva o arquivo pareado no terceiro

--- organizar_exames/pairing_progenos_univas.py
+++ organizar_exames/pairing_progenos_univas.py
@@ -1,25 +1,14 @@
 import pandas as pd
 
-def pair(data_spectra, data_univas):
-    univas_header = list(data_univas.columns)
-    spectra_headers = list(data_spectra.columns)
-    paired_headers = univas_header + ['Espectros'] + spectra_headers
-    print(paired_headers)
-    data_paired = pd.DataFrame(columns = paired_headers)
-
-    # for index_univas in range(0, len(data_univas)):
-    #     for index_spec in range(0, len(data_spectra)):
-    #         if data_univas.iloc[index_univas, 2] == data_spectra.iloc[index_spec, 1]:
-    #             print(data_univas.iloc[index_univas, 2])
-
-    return data_paired
+def pair(file_spectra, file_univas, file_output_pareado):
+    espectros = pd.read_csv(file_spectra)
+    exames_univas = pd.read_excel(file_univas)
+    resultado = pd.merge(exames_univas, espectros, on='Código', how='inner')
+    resultado.to_excel(file_output_pareado, index=False)
+    print(f"Planilha pareada com sucesso no diretório {file_output_pareado}")
 
 if __name__ == '__main__':
-    input_arquivo_spectras = input("Digite o caminho do arquivo com os espectros: ")
-    input_arquivo_univas = input("Digite o caminho do arquivo com os dados da univas: ")
-    output_arquivo_pareado = input("Digite o caminho de saída do arquivo pareado: ")
-    data_spectras = pd.read_csv(input_arquivo_spectras)
-    data_univas = pd.read_excel(input_arquivo_univas)
-    dados_pareados = pair(data_spectras, data_univas)
-    dados_pareados.to_csv(output_arquivo_pareado, index=False)
-    print (f'Arquivo {output_arquivo_pareado} pareado e salvo com sucesso')
+    espectros = input("Digite o caminho para o arquivo com os espectros: ")
+    exames_univas = input("Digite o caminho para o arquivo com os exames da univas mesclados: ")
+    pareado = input("Digite o caminho para o arquivo pareado ")
+    pair(espectros, exames_univas, pareado)
