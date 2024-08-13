96014c84156ae050232fdc54c28cd2aa5c0b0de3
viniciussm07 <vini.soares.martins@gmail.com>
2024-08-13 20:23:19 -0300

    Feat: começando implementação da fução changing_gasometrias_exams_strings

--- organizar_exames/txt_utils.py
+++ organizar_exames/txt_utils.py
@@ -17,6 +17,21 @@ def convert_to_utf8(input_file, output_file):
     with open(output_file, 'w', encoding='UTF-8') as outfile:
         outfile.write(content)
 
+def changing_gasometrias_exams_strings(input_file, output_file):
+    keyword = 'GASOMETRIAS'
+
+    dict_exams_venoso = {"BE": "GasometriaVenosaBE", "cHCO3": "GasometriaVenosacHCO3", "ctCO2": "GasometriaVenosactCO2", "FIO2": "GasometriaVenosaFIO2", "GLICEMIA:": "GasometriaVenosaGLICEMIA", "Hematócrito": "GasometriaVenosaHematócrito", "Lactato (ácido lático)": "GasometriaVenosaLactato", "pCO2": "GasometriaVenosapCO2", "pH": "GasometriaVenosapH", "pO2": "GasometriaVenosapO2", "sO2(c)": "GasometriaVenosasO2"}
+    dict_exams_arterial = {"BE": "GasometriaArterialBE", "cHCO3": "GasometriaArterialcHCO3", "ctCO2": "GasometriaArterialctCO2", "FIO2": "GasometriaArterialFIO2", "GLICEMIA:": "GasometriaArterialGLICEMIA", "Hematócrito": "GasometriaArterialHematócrito", "Lactato (ácido lático)": "GasometriaArterialLactato", "pCO2": "GasometriaArterialpCO2", "pH": "GasometriaArterialpH", "pO2": "GasometriaArterialpO2", "sO2(c)": "GasometriaArterialsO2"}
+
+    with open(input_file, 'r', encoding='ISO-8859-1') as infile:
+        lines = infile.readlines()
+
+    with open(output_file, 'w', encoding='UTF-8') as outfile:
+        for line in lines:
+            if keyword in line:
+                print("teste")
+
+
 if __name__ == '__main__':
     input_file_txt = input("Digite o caminho raiz dos arquivos txt organizados por dia: ")
     output_txt_sem_linhas_inuteis = input("Digite o caminho para o txt sem linhas inúteis: ")
