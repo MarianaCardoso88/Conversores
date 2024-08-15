d70eaf2f39005d2393be76f4f5c87e4d8874c0bc
viniciussm07 <vini.soares.martins@gmail.com>
2024-08-15 10:36:12 -0300

    Feat/Fix: função changing_gasometrias_exams_strings lida corretamente com gasometrias arterial e venosa / arrumando encoding de remove_lines_with_keywords

--- organizar_exames/txt_utils.py
+++ organizar_exames/txt_utils.py
@@ -2,10 +2,10 @@ import file_processing
 
 def remove_lines_with_keywords(input_file, output_file):
     keywords = ['Globulinas', 'urina', 'fezes', 'Urina', 'Fezes', 'URINA', 'FEZES', 'BIOQUÍMICA', 'HEMATOLOGIA', 'UROANÁLISE', 'MICROBIOLOGIA', 'Antibiograma', 'Pesquisa de BAAR', 'Bacterioscopia', 'Cultura', 'Hemocultura', 'HERMES PARDINI', 'Tacrolimus', 'Brucelose', 'Citomegalovirus', 'Hepatite', 'HIV', 'HTLV', 'Trypanosoma', 'IMUNOLOGIA', 'VDRL', 'ABO-RhD', 'Fator Rh', 'Pesquisa de D fraco', '%', 'LAB. APOIO DB', 'Relação TTPA/Controle', 'Glicemia Média Estimada', 'HEMODIÁLISE', 'PARASITOLOGIA']
-    with open(input_file, 'r', encoding='ISO-8859-1') as infile:
+    with open(input_file, 'r', encoding='UTF-8') as infile:
         lines = infile.readlines()
     
-    with open(output_file, 'w', encoding='utf-8') as outfile:
+    with open(output_file, 'w', encoding='UTF-8') as outfile:
         for line in lines:
             if not any(keyword in line for keyword in keywords):
                 outfile.write(line)
@@ -24,41 +24,39 @@ def changing_gasometrias_exams_strings(input_file, output_file):
     dict_exams_venoso = {"BE": "GasometriaVenosaBE", "cHCO3": "GasometriaVenosacHCO3", "ctCO2": "GasometriaVenosactCO2", "FIO2": "GasometriaVenosaFIO2", "GLICEMIA:": "GasometriaVenosaGLICEMIA", "Hematócrito": "GasometriaVenosaHematócrito", "Lactato (ácido lático)": "GasometriaVenosaLactato", "pCO2": "GasometriaVenosapCO2", "pH": "GasometriaVenosapH", "pO2": "GasometriaVenosapO2", "sO2(c)": "GasometriaVenosasO2"}
     dict_exams_arterial = {"BE": "GasometriaArterialBE", "cHCO3": "GasometriaArterialcHCO3", "ctCO2": "GasometriaArterialctCO2", "FIO2": "GasometriaArterialFIO2", "GLICEMIA:": "GasometriaArterialGLICEMIA", "Hematócrito": "GasometriaArterialHematócrito", "Lactato (ácido lático)": "GasometriaArterialLactato", "pCO2": "GasometriaArterialpCO2", "pH": "GasometriaArterialpH", "pO2": "GasometriaArterialpO2", "sO2(c)": "GasometriaArterialsO2"}
 
-    with open(input_file, 'r', encoding='ISO-8859-1') as infile:
+    with open(input_file, 'r', encoding='UTF-8') as infile:
         lines = infile.readlines()
 
-    # Itera sobre cada linha do arquivo
-    for i in range(len(lines)):
-        # Verifica se existe a linha escrito "GASOMETRIAS"
-        if keyword in lines[i]:
-            # pula a linha que contém a keyword
-            i += 1
-            # Itera sobre o restante das linhas do arquivo
-            for k in range(i, len(lines)):
-                # Condição para não alterar linhas depois das GASOMETRIAS
-                if (lines[k][0] == ' ') and (lines[k][1] == ' ') and (lines[k][2] == ' ') and (lines[k][3] == ' '):
-                    # Itera sobre as chaves dos dois dicionários que são iguais
-                    for j in range(len(dicts_keys)):
-                        if (dicts_keys[j] in lines[k]) and ('Venoso' in lines[k]):
-                            # Substituindo pela string correta
-                            lines[k].replace(dicts_keys[j], dict_exams_venoso[j])
-                            print('---- Venoso: ' + lines[k])
-                        elif (dicts_keys[j] in lines[k]) and ('Arterial' in lines[k]):
-                            # Substituir pela string correta
-                            lines[k].replace(dicts_keys[j], dict_exams_arterial[j])
-                            print('---- Arterial: ' + lines[k])
-                        # Faltou escrever no arquivo
-                else:
-                    break
-                i = k
-        # else:
-            # Faltou escrever no arquivo as linhas que não fazem parte das GASOMETRIAS
-            
-
-    # with open(output_file, 'w', encoding='UTF-8') as outfile:
-    #     for line in lines:
-    #         if keyword in line:
-    #             print("teste")
+    with open(output_file, 'w', encoding='UTF-8') as outfile:
+        # Itera sobre cada linha do arquivo
+        i = 0
+        while i < (len(lines) - 1):
+            # Verifica se existe uma linha com a keyword
+            if keyword in lines[i]:
+                # Pula a linha da keyword
+                i += 1
+                # Itera sobre o restante das linhas do arquivo
+                while i < (len(lines) - 1):
+                    # Condição para não alterar linhas depois das GASOMETRIAS
+                    if (lines[i][0] == ' ') and (lines[i][1] == ' ') and (lines[i][2] == ' ') and (lines[i][3] == ' '):
+                        # Itera sobre as chaves dos dois dicionários que são iguais
+                        for j in range(len(dicts_keys)):
+                            if (dicts_keys[j] in lines[i]) and ('Venoso' in lines[i]):
+                                # Substituindo pela string correta
+                                lines[i] = lines[i].replace(dicts_keys[j], dict_exams_venoso[dicts_keys[j]])
+                                outfile.write(lines[i])
+                            elif (dicts_keys[j] in lines[i]) and ('Arterial' in lines[i]):
+                                # Substituindo pela string correta
+                                lines[i] = lines[i].replace(dicts_keys[j], dict_exams_arterial[dicts_keys[j]])
+                                outfile.write(lines[i])
+                            # Faltou escrever no arquivo
+                    else:
+                        break
+                    i += 1
+            else:
+                # Faltou escrever no arquivo as linhas que não fazem parte das GASOMETRIAS
+                outfile.write(lines[i])
+                i += 1
 
 
 if __name__ == '__main__':
@@ -66,7 +64,7 @@ if __name__ == '__main__':
     # output_txt_sem_linhas_inuteis = input("Digite o caminho para o txt sem linhas inúteis: ")
     # file_processing.process_files(input_file_txt, output_txt_sem_linhas_inuteis, remove_lines_with_keywords)
     # print("Linhas removidas com sucesso!")]
-    input_file_txt = "./7516347"
+    input_file_txt = "./7516347_utf8"
     output_file_txt = "./7516347_tratado"
     changing_gasometrias_exams_strings(input_file_txt, output_file_txt)
     print('finalizado')
